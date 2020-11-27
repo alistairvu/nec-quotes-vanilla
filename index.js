2c43bb42c15a8cf4191cb6d0b486264f6579e0c5
Alistair Vu <alistairvu@gmail.com>
2020-11-27 16:32:29 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let quote = ""
+let author = ""
+const rootElement = document.getElementById("root")
+const url = "https://type.fit/api/quotes"
+
+const getQuotes = async () => {
+  rootElement.innerHTML = `
+    <p class="loading">Loading...</p>
+  `
+
+  const res = await fetch(url)
+  const data = await res.json()
+
+  const length = data.length
+  const randomQuote = data[Math.floor(Math.random() * length)]
+
+  quote = randomQuote.text
+  author = randomQuote.author
+  rootElement.innerHTML = `
+    <div class="container">
+      <p class="quote">"${quote}"</p>
+      <p class="author">${author !== null ? author : ""}</p>
+      <button class="quote-btn">
+        Get another quote
+      </button>
+    </div>
+  `
+  document.querySelector(".quote-btn").addEventListener("click", getQuotes)
+}
+
+getQuotes()
